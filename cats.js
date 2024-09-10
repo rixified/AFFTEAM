a4cb4a531bb7fe6b4f992879523b045be71eda91
Jenkins, Kenneth Alexander <kjenkins60@gatech.edu>
2024-09-10 01:29:56 -0400

    working 5 pics + description

--- cats.js
+++ cats.js
@@ -1,14 +1,12 @@
-const url = `https://api.thecatapi.com/v1/images/search?limit=4`; // Change limit to 5
-const api_key = "MM4ElURC9w1QaqI5S8X1PJOQ0xsHFr8h74ZAkz1YcYSUdPzeINbe5lo8nubku4ZR"
+const url = `https://api.thecatapi.com/v1/images/search?limit=5`; // Load 5 images
+const api_key = "MM4ElURC9w1QaqI5S8X1PJOQ0xsHFr8h74ZAkz1YcYSUdPzeINbe5lo8nubku4ZR";
 
 fetch(url, {
   headers: {
     'x-api-key': api_key
   }
 })
-.then((response) => {
-  return response.json();
-})
+.then((response) => response.json())
 .then((data) => {
   let imagesData = data;
   imagesData.map(function(imageData) {
@@ -16,16 +14,23 @@ fetch(url, {
     let image = document.createElement('img');
     image.src = `${imageData.url}`;
         
-    let textBox = document.createElement('input');
-    textBox.type = 'text';
-    textBox.classList.add('text-box');
-    textBox.placeholder = 'Enter text here';
+    let infoBox = document.createElement('div');
+    infoBox.classList.add('info-box');
+    
+    let header = document.createElement('h3');
+    header.innerText = 'Cat Image Info';
+    
+    let paragraph = document.createElement('p');
+    paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    
+    infoBox.appendChild(header);
+    infoBox.appendChild(paragraph);
 
     let gridCell = document.createElement('div');
     gridCell.classList.add('col');
     gridCell.classList.add('col-lg');
     gridCell.appendChild(image);
-    gridCell.appendChild(textBox);
+    gridCell.appendChild(infoBox);
       
     document.getElementById('grid').appendChild(gridCell);
     
